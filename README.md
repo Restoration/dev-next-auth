90836db4780c8c96b57bc221fb9898e71e8025a5
RyotArch <r.yamamoto.system@gmail.com>
2024-11-20 17:25:34 +0900

    feat

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 
 - https://docs.login.xyz/integrations/nextauth.js
+- https://stackoverflow.com/questions/68188861/next-auth-how-the-registration-is-handled-with-a-email-password-credential-p
--- src/app/components/LoginButton.tsx
+++ src/app/components/LoginButton.tsx
@@ -4,20 +4,24 @@ import { signIn } from "next-auth/react";
 
 // ログインボタン
 export const LoginButton = () => {
-  const handleLogin = async () => {
-    try {
-      const callbackUrl = "/protected";
-      signIn("credentials", {
-        redirect: false,
-        callbackUrl,
-      });
-    } catch (error) {
-      window.alert(error);
+  // フォーム送信処理
+  const onSubmit = async (data: { email: string, password: string }) => {
+
+    const result = await signIn('user', {
+      redirect: false,
+      email: data.email,
+      password: data.password
+    })
+
+    if (result?.error) {
+      alert("ログイン失敗")
+    } else {
+      location.href = '/dashboard'
     }
-  };
+  }
   return (
     <>
-      <button style={{ marginRight: 10 }} onClick={() => handleLogin()}>
+      <button style={{ marginRight: 10 }} onClick={() => onSubmit({ email: "user1@example.com", password: "password1" })}>
         Sign in
       </button>
       <button style={{ marginRight: 10 }} onClick={() => signIn()}>
